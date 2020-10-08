ab7c25bd2c86b7358b8485bbdd375c4eaacfd148
Luthando <lutsanmkhwa@gmail.com>
2020-10-08 11:44:40 +0200

    updated consoletodiv * 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = function consoletodiv(id) {
-    ['log','warn','error'].forEach(function (verb) {
-        let log = document.querySelector('#' + id);
+    const log = document.querySelector('#' + id);
+    ['log','debug','info','warn','error'].forEach(function (verb) {
         console[verb] = (function (method, verb, log) {
-            return function (text) {
-                method(text);
-                let msg = document.createElement('code');
+            return function () {
+                method.apply(console, arguments);
+                const msg = document.createElement('div');
                 msg.classList.add(verb);
-                msg.textContent = verb + ': ' + text + '\n';
+                msg.textContent = verb + ': ' + Array.prototype.slice.call(arguments).join(' ');
                 log.appendChild(msg);
             };
-        })(console[verb].bind(console), verb, log);
+        })(console[verb], verb, log);
     });
 };
